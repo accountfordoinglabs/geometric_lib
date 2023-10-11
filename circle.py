34de501de62b65075f3edc69d4d0ff9a472d0c95
accountfordoinglabs <144909109+accountfordoinglabs@users.noreply.github.com>
2023-10-11 14:02:50 +0300

    Add files via upload

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+'''Принимает число  r, возвращает площадь круга радиуса r'''
     return math.pi * r * r
 
 
 def perimeter(r):
+'''Принимает число  r, возвращает длину окружности радиуса r'''
     return 2 * math.pi * r
 
